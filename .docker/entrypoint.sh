00a66d0e3e9cd624102389b33ace4e7847afaf48
Fabian Bucher <git@fabianbucher.de>
2021-02-15 22:53:08 +0100

    docker: init

---
+++ .docker/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd /home/c3nav/c3nav
+. env/bin/activate
+cd src
+
+python --version
+
+if [ -z "$1" ]
+  then
+    python manage.py collectstatic --noinput
+    python manage.py compress
+    rm -rf /home/c3nav/static/*
+    cp -r /home/c3nav/c3nav/src/c3nav/static.dist/* /home/c3nav/static/
+    gunicorn --bind=0.0.0.0 c3nav.wsgi
+else
+  python manage.py $1
+fi
---
+++ .docker/readme.md
@@ -0,0 +1,21 @@
+# Docker
+
+## Setup
+
+run once (from `.docker` directory)
+
+```
+docker-compose run c3nav migrate
+```
+
+create an admin user
+
+```
+docker-compose run c3nav createsuperuser
+```
+
+## Run
+
+```
+docker-compose up -d
+```
